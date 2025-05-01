4c1a72fa6618b50020d99e4567671a44c4f5443a
saixrr <111263091+saixrr@users.noreply.github.com>
2025-05-01 23:25:05 +0530

    final

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
 import authRoutes from './routes/auth.js';
 import { connectToDB } from './db/connect.js';
+
+dotenv.config(); // load .env
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use('/api', authRoutes);
-connectToDB()
 
-app.listen(3000, () => {
-  console.log('🚀 Server running on http://localhost:3000');
+connectToDB();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
-  
